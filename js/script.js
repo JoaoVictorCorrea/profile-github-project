f2b5e9151f7a2dff053ee5cdf6509e1ae23b534c
João Victor Correa <correa.joaov25@gmail.com>
2024-01-18 16:28:54 -0300

    closes #3 - Error Handling

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
 function search() {
 
     var username = document.getElementById("inputUserName").value;
-
     var url = `https://api.github.com/users/${username}`;
     
     $.getJSON(url, (user) => {
 
-        document.getElementById("name").innerHTML = user.name;
-        document.getElementById("html_url").innerHTML = user.html_url;
-        document.getElementById("company").innerHTML = user.company;
+        showUserData(user);
+        clearError();
 
-        document.getElementById("avatar_url").innerHTML = `
-        <img src="${user.avatar_url}" width="220" height="220" class="shadow rounded">
-        `
+    }).fail(() => {
+
+        showUserData({});
+        showError("Não Encontrado!");
     });
+}
+
+function showUserData(user) {
+    document.getElementById("name").innerHTML = user.name || "";
+    document.getElementById("html_url").innerHTML = user.html_url || "";
+    document.getElementById("company").innerHTML = user.company || "";
+
+    document.getElementById("avatar_url").innerHTML = user.avatar_url ? `
+    <img src="${user.avatar_url}" width="220" height="220" class="shadow rounded">` : "";
+}
+
+function showError(msg) {
+    document.getElementById("error").innerHTML = `<div class='alert alert-danger mt-1' role='alert'>${msg}</div>`;
+}
+
+function clearError() {
+    document.getElementById("error").innerHTML = "";
 }
